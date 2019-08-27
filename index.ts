c4878820fdd694ee753593e7c928c34f8eb4c513
EdwardMwaura <edduemwaurer@outlook.com>
2019-08-27 17:51:29 +0300

    2 files modified

--- index.ts
+++ index.ts
@@ -3,4 +3,19 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>Alphabet Subsequence</h1>`;
+function alphabetSubsequence(s: string): boolean {
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  let arr = s.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if(alphabets.indexOf(arr[i]) < (alphabets.indexOf(arr[i])+1)){
+          console.log(arr[i]);
+    }
+  }
+}
+
+console.log(alphabetSubsequence('zab'))
+/* console.log(alphabetSubsequence('effg'))
+console.log(alphabetSubsequence('cdce'))
+console.log(alphabetSubsequence('ace'))
+console.log(alphabetSubsequence('bxz')) */
